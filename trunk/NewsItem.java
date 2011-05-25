aba0ec1a0d770d56a5527cdebbe4d48a99b1c268
craincat666@gmail.com <craincat666@gmail.com@d334de05-e4d8-903e-ec9d-a1e14748ca36>
2011-05-25 03:27:26 +0000

    Modified Alert

--- trunk/NewsItem.java
+++ trunk/NewsItem.java
@@ -16,14 +16,26 @@ import java.text.SimpleDateFormat;
 
 class NewsItem
 {
-	String source;
-	Date date;
-	String title;
-	String body;
+	public String source;
+	public Date date;
+	public String title;
+	public String body;
+
+	private static String SEP = "_NEWSSEP_";
+
+	private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
 	
 	NewsItem(String str)
 	{
 		decode(str);
+	}
+
+	NewsItem(String source, Date date, String title, String body)
+	{
+		this.source = source;
+		this.date = date;
+		this.title = title;
+		this.body = body;
 	}
 			
 	public void decode(String str)
@@ -31,7 +43,6 @@ class NewsItem
 		source = getTagData("nsource", str);
 		try
 		{
-			SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
 			date = sdf.parse(getTagData("ndate", str));
 		}
 		catch (Exception e)
@@ -65,8 +76,62 @@ class NewsItem
 	public void print()
 	{
 		System.out.println("Source: " + source);
-		System.out.println("Date: " + date.toString());
+		System.out.println("Date: " + sdf.format(date));
 		System.out.println("Title: " + title);
 		System.out.println("Body: " + body);
+	}
+
+	// Decodes a news from String format
+	// Returns the decoded news
+	public static NewsItem decodeMsg(String msg)
+	{
+		// Split the message up into an array
+		String[] bit = msg.split(SEP);
+
+		NewsItem decoded;
+
+		try
+		{
+		// Create a new news
+		decoded = new NewsItem(bit[0], sdf.parse(bit[1]), bit[2], bit[3]);
+		return decoded;
+		}
+		catch (Exception e) { return null; }
+	}
+
+	public static NewsItem[] decodeArray(String[] msg)
+	{
+		NewsItem[] news = new NewsItem[msg.length];
+		for (int i = 0; i < msg.length; i ++)
+		{
+			news[i] = decodeMsg(msg[i]);
+		}
+		return news;
+	}
+
+	// Encodes an news into string format
+	// Returns the encoded news
+	public String encode()
+	{
+		// Transform this news into a string
+		String encoded = 
+			source + SEP +
+			sdf.format(date) + SEP + 
+			title + SEP +
+			body;
+
+		// Return the encoded news
+		return encoded;
+	}
+
+	public static String[] encodeArray(NewsItem[]  news)
+	{
+		String[] encoded = new String[news.length];
+		for (int i = 0; i < news.length; i ++)
+		{
+			encoded[i] = news[i].encode();
+		}
+
+		return encoded;
 	}
 }
