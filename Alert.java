e42f0f39af6f99b8860bbf60aa95f241c6c1b069
craincat666 <craincat666@gmail.com>
2011-05-25 03:27:26 +0000

    Modified Alert

--- Alert.java
+++ Alert.java
@@ -1,27 +1,32 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 // Holds all the data for an alert
 class Alert
 {
 	// Alert details
 	public String id;
-	public String time;
-	public String points;
-	public String[] rules;
-	public String[] news;
-	public String[] companies;
+	public Date time;
+	public Integer points;
+	public String company;
+	public RuleItem[] rules;
+	public NewsItem[] news;
 
 	// Message encoding variables
-	private static String SEP = "{sep}";
-	private static String SUBSEP = "{subsep}";
+	private static String SEP = "_SEP_";
+	private static String SUBSEP = "_SUBSEP_";
+
+	private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 
 	// Constructor
-	Alert(String id,  String time, String points, String[] rules, String[] news, String[] companies)
+	Alert(String id,  Date time, Integer points, String company, RuleItem[] rules, NewsItem[] news)
 	{
 		this.id = id;
 		this.time = time;
 		this.points = points;
 		this.rules = rules;
 		this.news = news;
-		this.companies = companies;
+		this.company = company;
 	}
 
 	// Decodes an alert from String format
@@ -30,12 +35,19 @@ class Alert
 	{
 		// Split the message up into an array
 		String[] bit = msg.split(SEP);
-
-		// Create a new alert
-		Alert decoded = new Alert(bit[0], bit[1], bit[2], bit[3].split(SUBSEP), bit[4].split(SUBSEP), bit[5].split(SUBSEP));
-
-		// Return decoded alert
-		return decoded;
+		try 
+		{
+			// Create a new alert
+			Alert decoded = new Alert(
+				bit[0],
+				sdf.parse(bit[1]),
+				Integer.parseInt(bit[2]),
+				bit[3],
+				RuleItem.decodeArray(bit[4].split(SUBSEP)),
+				NewsItem.decodeArray(bit[5].split(SUBSEP)));
+			return decoded;
+		}
+		catch (Exception e) {return null;}
 	}
 
 	// Encodes an alert into string format
@@ -45,11 +57,11 @@ class Alert
 		// Transform this alert into a string
 		String encoded = 
 			id + SEP + 
-			time + SEP + 
-			points + SEP + 
-			Util.implode(rules, SUBSEP) + SEP + 
-			Util.implode(news, SUBSEP) + SEP + 
-			Util.implode(companies, SUBSEP);
+			sdf.format(time) + SEP + 
+			points.toString() + SEP + 
+			company + SEP + 
+			Util.implode(RuleItem.encodeArray(rules), SUBSEP) + SEP + 
+			Util.implode(NewsItem.encodeArray(news), SUBSEP);
 
 		// Return the encoded alert
 		return encoded;
