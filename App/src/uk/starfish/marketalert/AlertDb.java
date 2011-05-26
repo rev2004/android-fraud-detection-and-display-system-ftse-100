402eef512389f4ca731d49d9758ec4865b596394
craincat666 <craincat666@gmail.com>
2011-05-26 04:46:44 +0000

    Added an alert database (text file implementation)

---
+++ App/src/uk/starfish/marketalert/AlertDb.java
@@ -0,0 +1,75 @@
+package uk.starfish.marketalert;
+
+import java.io.*;
+import java.util.*;
+
+public class AlertDb
+{
+	private List<Alert> alerts;
+	public static String SEP = "_ALERTSEP_";
+	public static String FILENAME = "alerts.txt";
+	
+	public AlertDb()
+	{
+		this.alerts = new ArrayList<Alert>();
+	}
+
+	public void save()
+	{
+		try
+		{
+			File file = new File(FILENAME);
+			ReadWriteTextFile.setContents(file, encode()); 
+		}
+		catch (Exception e) { e.printStackTrace(); }
+	}
+
+	public void load()
+	{
+		File file = new File(FILENAME);
+		String contents = ReadWriteTextFile.getContents(file);
+		decode(contents);
+	}
+
+	public void add(Alert alert)
+	{
+		alerts.add(alert);
+	}
+
+	public void decode(String s)
+	{
+		alerts.clear();
+
+		if (s.length() > 5)
+		{
+			String[] alertStr = s.split(SEP);
+
+			for (int i = 0; i < alertStr.length; i ++)
+			{
+				alerts.add(Alert.decode(alertStr[i]));
+			}
+		}
+
+		System.out.print("Loaded ");
+		System.out.print(alerts.size());
+		System.out.println(" alerts");
+	}
+
+	public String encode()
+	{
+		if (alerts.isEmpty())
+			return "";
+		else
+		{
+			String[] result = new String[alerts.size()];
+			for (int i = 0; i < alerts.size(); i ++)
+			{
+				Alert alert = (Alert) alerts.get(i);
+				String e = alert.encode();
+				result[i] = e;
+			}
+
+			return Util.implode(result, SEP);
+		}
+	}
+}
